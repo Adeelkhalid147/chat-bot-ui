340d1b3a14d3bcebf07ab30725aaf151f986c889
AdeelKhalid147 <adeelkhalid147@gmail.com>
2025-05-01 17:27:39 +0500

    changes in both next config files

--- next.config.mjs
+++ next.config.mjs
@@ -8,16 +8,15 @@
 
 // export default nextConfig;
 
+const abc = {
+  async rewrites() {
+    return [
+      {
+        source: "/chat",
+        destination: "http://152.53.229.172:5010/chat",
+      },
+    ];
+  },
+};
 
-
-
-export default {
-    async rewrites() {
-      return [
-        {
-          source: '/chat',
-          destination: 'http://152.53.229.172:5010/chat',
-        },
-      ];
-    },
-  };
+export default abc;
